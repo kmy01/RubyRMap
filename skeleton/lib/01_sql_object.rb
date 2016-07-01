75c95afb9242206ad69e00a00c8a63601a230855
Kei Man Yeung <ky2286@columbia.edu>
2016-07-01 14:07:13 -0400

    create getters and setters for attributes

--- skeleton/lib/01_sql_object.rb
+++ skeleton/lib/01_sql_object.rb
@@ -5,25 +5,32 @@ require 'active_support/inflector'
 
 class SQLObject
   def self.columns
-    if instance_variable_get('@columns').nil?
-      c = DBConnection.execute2("SELECT * FROM #{table_name}")
-      instance_variable_set("@columns", c.first.map { |column| column.to_sym })
-    end
-    instance_variable_get('@columns')
-
+    # if instance_variable_get('@columns').nil?
+    #   c = DBConnection.execute2("SELECT * FROM #{table_name}")
+    #   instance_variable_set("@columns", c.first.map { |column| column.to_sym })
+    # end
+    # instance_variable_get('@columns')
+    @columns ||= DBConnection.execute2("SELECT * FROM #{table_name}")
+      .first.map { |column| column.to_sym }
   end
 
   def self.finalize!
+    self.columns.each do |column|
+      define_method(column) { self.attributes[column]} # getter
+      define_method("#{column}=") { |arg| self.attributes[column] = arg} # setter
+    end
   end
 
   def self.table_name=(table_name)
-    instance_variable_set("@table_name", table_name)
+    # instance_variable_set("@table_name", table_name)
+    @table_name = table_name
   end
 
   def self.table_name
-    get = instance_variable_get("@table_name")
-    instance_variable_set("@table_name", self.to_s.tableize) if get.nil?
-    instance_variable_get("@table_name")
+    # get = instance_variable_get("@table_name")
+    # instance_variable_set("@table_name", self.to_s.tableize) if get.nil?
+    # instance_variable_get("@table_name")
+    @table_name ||= self.to_s.tableize
   end
 
   def self.all
@@ -39,11 +46,11 @@ class SQLObject
   end
 
   def initialize(params = {})
-    # ...
+    
   end
 
   def attributes
-    # ...
+    @attributes ||= {}
   end
 
   def attribute_values
