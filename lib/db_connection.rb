1668cb4a1692c180610b45df235e2a6fc7d7136d
Kei Man Yeung <ky2286@columbia.edu>
2016-08-16 17:04:08 -0400

    update database files to reflect change to pets

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -1,10 +1,9 @@
 require 'sqlite3'
 
 PRINT_QUERIES = ENV['PRINT_QUERIES'] == 'true'
-# https://tomafro.net/2010/01/tip-relative-paths-with-file-expand-path
 ROOT_FOLDER = File.join(File.dirname(__FILE__), '..')
-CATS_SQL_FILE = File.join(ROOT_FOLDER, 'cats.sql')
-CATS_DB_FILE = File.join(ROOT_FOLDER, 'cats.db')
+SQL_FILE = File.join(ROOT_FOLDER, 'pets.sql')
+DB_FILE = File.join(ROOT_FOLDER, 'pets.db')
 
 class DBConnection
   def self.open(db_file_name)
@@ -17,12 +16,12 @@ class DBConnection
 
   def self.reset
     commands = [
-      "rm '#{CATS_DB_FILE}'",
-      "cat '#{CATS_SQL_FILE}' | sqlite3 '#{CATS_DB_FILE}'"
+      "rm '#{DB_FILE}'",
+      "cat '#{SQL_FILE}' | sqlite3 '#{DB_FILE}'"
     ]
 
     commands.each { |command| `#{command}` }
-    DBConnection.open(CATS_DB_FILE)
+    DBConnection.open(DB_FILE)
   end
 
   def self.instance
