333d0cdcc47d085b13c868c308c17c99510a7d27
Kei Man Yeung <ky2286@columbia.edu>
2016-08-16 15:03:08 -0400

    complete has_one_through association

---
+++ lib/associatable.rb
@@ -0,0 +1,79 @@
+require_relative 'searchable'
+require 'active_support/inflector'
+
+class AssocOptions
+  attr_accessor(
+    :foreign_key,
+    :class_name,
+    :primary_key
+  )
+
+  def model_class
+    class_name.constantize
+  end
+
+  def table_name
+    model_class.table_name
+  end
+end
+
+class BelongsToOptions < AssocOptions
+  def initialize(name, options = {})
+    options = {
+      foreign_key: "#{name}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.camelcase
+    }.merge(options)
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
+  end
+end
+
+class HasManyOptions < AssocOptions
+  def initialize(name, self_class_name, options = {})
+    options = {
+      foreign_key: "#{self_class_name.downcase}_id".to_sym,
+      primary_key: :id,
+      class_name: name.to_s.singularize.camelcase
+    }.merge(options)
+
+    @foreign_key = options[:foreign_key]
+    @primary_key = options[:primary_key]
+    @class_name = options[:class_name]
+  end
+end
+
+module Associatable
+  def belongs_to(name, options = {})
+    self.assoc_options[name] = BelongsToOptions.new(name, options)
+    options = assoc_options[name]
+
+    define_method(name) do
+      foreign_key_val = self.send(options.foreign_key)
+      model_class = options.model_class
+
+      model_class.where(options.primary_key => foreign_key_val).first
+    end
+  end
+
+  def has_many(name, options = {})
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      foreign_key_vals = self.send(options.primary_key)
+      model_class = options.model_class
+
+      model_class.where(options.foreign_key => foreign_key_vals)
+    end
+  end
+
+  def assoc_options
+    @assoc_options ||= {}
+  end
+end
+
+class SQLObject
+  extend Associatable
+end
---
+++ lib/searchable.rb
@@ -0,0 +1,15 @@
+require_relative 'db_connection'
+require_relative 'sql_object'
+module Searchable
+  def where(params)
+    table_name = self.table_name
+    where_line = params.keys.map { |attr_name| "#{attr_name} = ? " }.join("AND ")
+    found_data = DBConnection
+      .execute("SELECT * FROM #{table_name} WHERE #{where_line}", *params.values)
+    found_data.map { |data| self.new(data)}
+  end
+end
+
+class SQLObject
+  extend Searchable
+end
