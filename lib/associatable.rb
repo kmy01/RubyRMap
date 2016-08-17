0ec0482ea08b2cda0453256f580639a578c1c5cc
Kei Man Yeung <ky2286@columbia.edu>
2016-08-16 20:05:04 -0400

    fix has_one_through query

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -73,22 +73,32 @@ module Associatable
     through_options = assoc_options[through_name]
     source_options = through_options.model_class.assoc_options[source_name]
 
+    through_table = through_options.table_name
+    through_foreign_key = through_options.foreign_key
+    through_primary_key = through_options.primary_key
+
+    source_table = source_options.table_name
+    source_foreign_key = source_options.foreign_key
+    source_primary_key = source_options.primary_key
+
+
+
     define_method(name) do
-      through_foreign_key_val = self.send(through_options.foreign_key)
-      through_primary_key = through_options.primary_key
-      through_class = through_options.model_class
+      values = self.send(through_foreign_key)
+      results = DBConnection.execute(
+        "SELECT
+          #{source_table}.*
+        FROM
+          #{through_table}
+        JOIN
+          #{source_table}
+        ON
+          #{source_table}.#{through_primary_key} = #{through_table}.#{source_foreign_key}
+        WHERE
+          #{through_table}.#{through_primary_key} = ?", *values
+      )
 
-      source_obj = through_class.where(
-        through_primary_key => through_foreign_key_val
-      ).first
-
-      source_foreign_key_val = source_obj.send(source_options.foreign_key)
-      source_primary_key = source_options.primary_key
-      source_class = source_options.model_class
-
-      source_class.where(
-        source_primary_key => source_foreign_key_val
-      ).first
+      source_options.model_class.parse_all(results).first
     end
   end
 
